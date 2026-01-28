60ecd5bcaa56148a9041d650bee2045cb635908c
Girts Silis <girts.silis@gmail.com>
2026-01-28 09:54:20 -0600

    + Resources listed

--- src/resources.ts
+++ src/resources.ts
@@ -2,7 +2,15 @@ import { ImageSource, Loader } from "excalibur";
 
 // It is convenient to put your resources in one place
 export const Resources = {
-  Sword: new ImageSource("./images/sword.png") // Vite public/ directory serves the root images
+  // Sword: new ImageSource("./images/sword.png") // Vite public/ directory serves the root images
+  Background: new ImageSource("./images/background.png"),
+  Drone: new ImageSource("./images/drone.png"),
+  Missile: new ImageSource("./images/missile.png"),
+  Plane: new ImageSource("./images/plane.png"),
+  PlaneFlame: new ImageSource("./images/plane-flame.png"),
+  PlaneFlameBoost: new ImageSource("./images/plane-flame-boost.png"),
+  Ship: new ImageSource("./images/ship.png"),
+  Shots: new ImageSource("./images/shots.png"),
 } as const; // the 'as const' is a neat typescript trick to get strong typing on your resources. 
 // So when you type Resources.Sword -> ImageSource
 
