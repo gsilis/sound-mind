950f992726d637da514ab80543734be5292f20e0
Girts Silis <girts.silis@gmail.com>
2026-01-29 18:57:31 -0600

    + background animated

--- src/level.ts
+++ src/level.ts
@@ -1,10 +1,14 @@
-import { BoundingBox, Color, Engine, Scene } from "excalibur";
+import { Actor, BoundingBox, Canvas, Color, Engine, ExcaliburGraphicsContext, Scene } from "excalibur";
 import { Player } from "./player";
 import { LevelWidget } from "./boost-level";
+import { Resources } from "./resources";
 
 export class MyLevel extends Scene {
   private player?: Player
   private boostLevel?: LevelWidget
+  private background: Actor = new Actor()
+  private backgroundCanvas?: Canvas
+  private backgroundOffset = 0
   backgroundColor = Color.Azure
 
   override onInitialize(engine: Engine): void {
@@ -13,6 +17,24 @@ export class MyLevel extends Scene {
     const halfWidth = engine.screen.halfCanvasWidth
     const height = engine.screen.height
     const third = height / 3
+
+    this.backgroundCanvas = new Canvas({
+      width: engine.canvasWidth * 2,
+      height: engine.canvasHeight * 2,
+      draw: (context) => {
+        const pattern = context.createPattern(Resources.Background.image, 'repeat')
+
+        if (pattern) {
+          context.fillStyle = pattern
+          context.fillRect(0, 0, engine.canvasWidth * 2, engine.canvasHeight * 2)
+        }
+      }
+    })
+    this.background.graphics.use(this.backgroundCanvas)
+    this.background.pos.x = engine.screen.canvasWidth
+    this.background.pos.y = engine.screen.canvasHeight
+    this.add(this.background)
+
     this.player = new Player(new BoundingBox(20, third, width - 40, height - 100))
     this.player.pos.x = halfWidth
     this.player.pos.y = height - 100
@@ -28,5 +50,12 @@ export class MyLevel extends Scene {
     if (this.boostLevel && this.player) {
       this.boostLevel.level = this.player.boostLevel
     }
+
+    this.backgroundOffset += (elapsedMs / 10)
+    this.backgroundOffset = this.backgroundOffset % 64
+    const widthOffset = engine.screen.canvasWidth - ((this.player?.pos.x || 0) / 3)
+
+    this.background.pos.y = this.backgroundOffset
+    this.background.pos.x = widthOffset
   }
 }
